aed7b657d4448447e04a6ce2dd2b097c72d3430e
Dumitru Cantea <canteadumitru@gmail.com>
2017-02-23 11:31:57 +0200

    test doc.referrer

--- src/js/ads.js
+++ src/js/ads.js
@@ -5,25 +5,11 @@
 		return;
 	}
 
-	console.log('url', loc.href);
-
-	function getQ(name) {
-		var query = loc.search.substring(1);
-		var vars = query.split('&');
-		for (var i = 0; i < vars.length; i++) {
-			var pair = vars[i].split('=');
-			if (pair[0] === name) {
-				return pair[1];
-			}
-		}
-		return (false);
-	}
+	// console.log('url', loc.href, doc.referrer);
 
 	function getParams() {
 		var params = {
-			url: getQ('url'),
-			h: getQ('h'),
-			w: getQ('w')
+			url: doc.referrer
 		};
 
 		if (params.url) {
